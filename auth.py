edd22fc06d7ebdebed4b53b355c3a831a82b80a8
deepakbpit2504 <dudejadeepak22@gmail.com>
2026-03-24 20:43:36 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,14 +1,7 @@
 from database import add_user, validate_user
 
 def register_user(username, password):
-    """
-    Registers a new user
-    """
     add_user(username, password)
-    return True
 
 def login_user(username, password):
-    """
-    Validates user login
-    """
     return validate_user(username, password)

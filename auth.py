2a4fa135d82b789b45485f5daf57e7eebd9c9afb
deepakbpit2504 <dudejadeepak22@gmail.com>
2026-03-24 08:05:20 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,25 +2,25 @@ import streamlit as st
 from database import add_user, validate_user
 
 def login():
-    st.subheader("🔐 Login")
+    st.markdown("## 🔐 Login")
 
-    username = st.text_input("Username", key="login_user")
-    password = st.text_input("Password", type="password", key="login_pass")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
     if st.button("Login"):
         if validate_user(username, password):
             st.session_state["user"] = username
-            st.success("Login successful!")
+            st.success("✅ Login successful")
         else:
-            st.error("Invalid credentials")
+            st.error("❌ Invalid credentials")
 
 
 def register():
-    st.subheader("📝 Register")
+    st.markdown("## 📝 Register")
 
-    username = st.text_input("New Username", key="reg_user")
-    password = st.text_input("New Password", type="password", key="reg_pass")
+    username = st.text_input("New Username")
+    password = st.text_input("New Password", type="password")
 
     if st.button("Register"):
         add_user(username, password)
-        st.success("User registered successfully!")
+        st.success("✅ User registered successfully")

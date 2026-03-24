6413857f5757ce147de0b4da91f547c5e755cd04
deepakbpit2504 <dudejadeepak22@gmail.com>
2026-03-24 20:43:22 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,37 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect("echo.db", check_same_thread=False)
-cursor = conn.cursor()
+conn = sqlite3.connect("users.db", check_same_thread=False)
+cur = conn.cursor()
 
 def init_db():
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        username TEXT,
-        password TEXT
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS emotions (
-        username TEXT,
-        text TEXT,
-        emotion TEXT
-    )
-    """)
+    cur.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS emotions(username TEXT, text TEXT, emotion TEXT)")
     conn.commit()
 
+init_db()
+
 def add_user(username, password):
-    cursor.execute("INSERT INTO users VALUES (?, ?)", (username, password))
+    cur.execute("INSERT INTO users VALUES (?, ?)", (username, password))
     conn.commit()
 
 def validate_user(username, password):
-    cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
-    return cursor.fetchone() is not None
+    cur.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+    return cur.fetchone() is not None
 
 def save_emotion(username, text, emotion):
-    cursor.execute("INSERT INTO emotions VALUES (?, ?, ?)", (username, text, emotion))
+    cur.execute("INSERT INTO emotions VALUES (?, ?, ?)", (username, text, emotion))
     conn.commit()
 
 def get_emotions(username):
-    cursor.execute("SELECT emotion FROM emotions WHERE username=?", (username,))
-    return cursor.fetchall()
+    cur.execute("SELECT emotion FROM emotions WHERE username=?", (username,))
+    return cur.fetchall()
